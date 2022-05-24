12d37406faf0b7b32851864caf0fb865e52e8be3
Humusk <gguixe23@gmail.com>
2022-05-25 00:02:06 +0200

    main menu finished

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public TextMeshProUGUI creditsText;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        if(creditsText.enabled == true)
+        {
+            creditsText.enabled = false;
+        }
+        else
+        {
+            creditsText.enabled = true;
+        }
+    }
+
+}
